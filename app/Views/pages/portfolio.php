f2091e4cbdb3f6880fb862c581e1c18af0af11db
Juanma Tola <tolajuanmanuel@gmail.com>
2020-10-13 20:28:42 -0300

    Separo vistas generales del Front-end del sitio

---
+++ app/Views/pages/portfolio.php
@@ -0,0 +1,77 @@
+<section id="portfolio_galery">
+        <div class="container">
+            <div class="row">
+                <div class="col ">
+                    <h1 class="text-center my-3"> Portfolio </h1>
+                </div>
+            </div>
+        </div>
+            <div class="container" id="galeria">
+                <div class="row">
+                    <div class="col-12 col-sm-4 imagen" >
+                        <div class="card" style="background-image: url(assets/img/example1.jpg); height: 300px; background-position: center; background-size: cover;">
+                            <!-- <img class="card-img-top" src="img/example1.jpg" alt="Card image cap"> -->
+                            <div class="card-footer bg-light" style="margin-top: 250px;">
+                              <small class="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-sm-4 imagen" >
+                        <div class="card" style="background-image: url(assets/img/example2.jpg); height: 300px; background-position: center; background-size: cover;">
+                            <!-- <img class="card-img-top" src="img/example1.jpg" alt="Card image cap"> -->
+                            <div class="card-footer bg-light" style="margin-top: 250px; border: none;">
+                              <small class="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- <div class="col-12 col-sm-4">
+                        <div class="card">
+                            <img class="card-img-top" src="img/example1.jpg" alt="Card image cap">
+                            <div class="card-footer">
+                              <small class="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                    </div> -->
+
+                    <div class="col-12 col-sm-4" >
+                        <div class="card">
+                            <img class="card-img-top" src="assets/img/example2.jpg" alt="Card image cap">
+                            <div class="card-footer">
+                              <small class="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <div class="col-12 col-sm-4 imagen">
+                        <div class="card ">
+                            <img class="card-img-top" src="assets/img/example2.jpg" alt="Card image cap">
+                            <div class="card-footer">
+                              <small class="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-sm-4">
+                        <div class="card">
+                            <img class="card-img-top" src="assets/img/example2.jpg" alt="Card image cap">
+                            <div class="card-footer">
+                              <small class="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-sm-4">
+                        <div class="card">
+                            <img class="card-img-top" src="assets/img/example2.jpg" alt="Card image cap">
+                            <div class="card-footer">
+                              <small class="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -1,9 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
+    <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/home.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 <body>
+    <!-- NAVBAR DE BOOTSTRAP -->
+    <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">Tu-Porfolio</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+    
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="/portfolio">Portfolio</a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="/contacto" tabindex="-1" aria-disabled="true">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     
