5ca605358f67e617234a4f62c2bfc35da4c1f0a0
Juanma Tola <tolajuanmanuel@gmail.com>
2020-10-13 02:57:40 -0300

    Seteo developer env y Creo estructura basica de vista/controlador

--- app/Controllers/Prueba.php
+++ app/Controllers/Prueba.php
@@ -4,11 +4,16 @@ class Prueba extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+		//return view('welcome_message');
+        echo 'esta es la función index';
     }
     
     public function test() {
-        echo 'esto es una prueba';
+        echo 'esto es la función test';
+    }
+
+    public function test2() {
+        echo 'esto es la función test2';
     }
 
 	//--------------------------------------------------------------------
