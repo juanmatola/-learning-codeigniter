483ec0ba2ea68cf5662c91b8c53702c774b11529
Juanma Tola <tolajuanmanuel@gmail.com>
2020-10-13 21:49:45 -0300

    logica con for para mostrar imagenes

--- app/Views/pages/portfolio.php
+++ app/Views/pages/portfolio.php
@@ -8,59 +8,17 @@
         </div>
             <div class="container" id="galeria">
                 <div class="row">
-                    <div class="col-12 col-sm-4 imagen" >
-                        <div class="card" style="background-image: url(assets/img/example1.jpg); height: 300px; background-position: center; background-size: cover;">
-                            <!-- <img class="card-img-top" src="img/example1.jpg" alt="Card image cap"> -->
-                            <div class="card-footer bg-light" style="margin-top: 250px;">
-                              <small class="text-muted">Last updated 3 mins ago</small>
-                            </div>
-                        </div>
-                    </div>
 
-                    <div class="col-12 col-sm-4 imagen" >
-                        <div class="card" style="background-image: url(assets/img/example2.jpg); height: 300px; background-position: center; background-size: cover;">
-                            <!-- <img class="card-img-top" src="img/example1.jpg" alt="Card image cap"> -->
-                            <div class="card-footer bg-light" style="margin-top: 250px; border: none;">
-                              <small class="text-muted">Last updated 3 mins ago</small>
+                    <?php for ($i=0; $i < 6 ; $i++) { ?>
+                        <div class="col-12 col-sm-4 mb-4">
+                            <div class="card portfolio-card" style="background-image: url(assets/img/example1.jpg);">
+                                <div class="card-footer bg-light" style="margin-top: 250px;">
+                                <small class="text-muted">Last updated <?php echo $i?> mins ago</small>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    <?php }  ?>
 
-                    <div class="col-12 col-sm-4" >
-                        <div class="card">
-                            <img class="card-img-top" src="assets/img/example2.jpg" alt="Card image cap">
-                            <div class="card-footer">
-                              <small class="text-muted">Last updated 3 mins ago</small>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-12 col-sm-4 imagen">
-                        <div class="card ">
-                            <img class="card-img-top" src="assets/img/example2.jpg" alt="Card image cap">
-                            <div class="card-footer">
-                              <small class="text-muted">Last updated 3 mins ago</small>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-12 col-sm-4">
-                        <div class="card">
-                            <img class="card-img-top" src="assets/img/example2.jpg" alt="Card image cap">
-                            <div class="card-footer">
-                              <small class="text-muted">Last updated 3 mins ago</small>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-12 col-sm-4">
-                        <div class="card">
-                            <img class="card-img-top" src="assets/img/example2.jpg" alt="Card image cap">
-                            <div class="card-footer">
-                              <small class="text-muted">Last updated 3 mins ago</small>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
     </section>
