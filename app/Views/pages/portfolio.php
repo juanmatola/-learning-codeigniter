e95ebe4690cd1612a037335ca90fb715be265160
Juanma Tola <tolajuanmanuel@gmail.com>
2020-10-14 19:21:05 -0300

    Cambios en vista de portfolio

--- app/Views/pages/portfolio.php
+++ app/Views/pages/portfolio.php
@@ -1,4 +1,4 @@
-<section id="portfolio_galery">
+<section id="portfolio_gallery">
         <div class="container">
             <div class="row">
                 <div class="col ">
@@ -6,15 +6,22 @@
                 </div>
             </div>
         </div>
-            <div class="container" id="galeria">
+            <div class="container" id="gallery">
                 <div class="row">
 
                     <?php for ($i=0; $i < 6 ; $i++) { ?>
-                        <div class="col-12 col-sm-4 mb-4">
+                        <!-- <div class="col-12 col-sm-4 mb-4">
                             <div class="card portfolio-card" style="background-image: url(assets/img/example1.jpg);">
                                 <div class="card-footer bg-light" style="margin-top: 250px;">
                                 <small class="text-muted">Last updated <?php echo $i?> mins ago</small>
                                 </div>
+                            </div>
+                        </div> -->
+
+                        <div class="col-12 col-sm-6 col-lg-4 my-3">
+                            <div class="gallery_image" style="background-image: url(assets/img/example1.jpg);"></div>
+                            <div class="card-footer">
+                                <small class="text-muted"> Titulo 1 </small>
                             </div>
                         </div>
                     <?php }  ?>
