1f671b7560edc6fa591c36b86b0268dc586ddee1
Juanma Tola <tolajuanmanuel@gmail.com>
2020-10-14 21:08:14 -0300

    Seteo ruta para administracion y primer esqueleto de admin

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -2,10 +2,29 @@
 
 class Admin extends BaseController
 {
+
 	public function index()
 	{
+		/*Si ya esta logueado, redirecciono a administración*/
+		if( false ){
+			return redirect()->to(base_url().'/admin/panel/');
+			exit;
+		}
+
 		echo view('admin/templates/header');
 		echo view('admin/login');
+		echo view('admin/templates/footer');
+		
+	}
+
+	public function panel(){
+		if (false) {
+			return redirect()->to(base_url().'/admin/');
+			exit;
+		}
+
+		echo view('admin/templates/header');
+		echo "<h1>BIENVENIDO A LA ADMINISTRACION<h1>";
 		echo view('admin/templates/footer');
 	}
 }
