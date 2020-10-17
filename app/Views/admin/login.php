4ca6aede0da95f24d5ab9031d8375b629c45df1d
Juan Manuel Tola <tolajuanmanuel@yahoo.com.ar>
2020-10-17 01:05:26 -0300

    Muestro inputs de formulario con funcion de helper de ci en vista de login

--- app/Views/admin/login.php
+++ app/Views/admin/login.php
@@ -14,19 +14,24 @@
         <div class="container h-100 d-flex flex-column justify-content-center align-items-center">
             <div class="login__container fade">
                 <img src="<?php echo base_url().'/assets/img/admin/athomic_logo.png'; ?>" alt="" class="login__container--img">
+                <?php echo form_open() ?>
+                
                 <form action="" method="POST">
                     <div class="form-group">
-                        <input type="email" class="form-control" placeholder="Usuario" id="exampleInputEmail1" aria-describedby="emailHelp" autocomplete="on">
+                        <!-- <input type="text" class="form-control" placeholder="Usuario" id="exampleInputEmail1" aria-describedby="emailHelp" autocomplete="on"> -->
+                        <?php echo form_input(array('name'=>'username','type'=>'text','class'=>'form-control', 'placeholder'=>'Usuario', 'autocomplete'=>'on'))?>
                         <small id="emailHelp" class="form-text text-muted">Nunca compartiremos su correo electrónico con nadie más.</small>
                     </div>
                     <div class="form-group">
-                        <input type="password" class="form-control" placeholder="Contraseña" id="exampleInputPassword1" autocomplete="on">
+                        <!-- <input type="password" class="form-control" placeholder="Contraseña" id="exampleInputPassword1" autocomplete="on"> -->
+                        <?php echo form_input(array('name'=>'password','type'=>'password','class'=>'form-control', 'placeholder'=>'Contraseña', 'autocomplete'=>'on'))?>
                     </div>
                     <div class="text-center form__buttons">
                         <a href="/">Regresar</a>
                         <button type="submit" class="btn btn-primary">Ingresar</button>
                     </div>
                 </form>
+                <?php echo form_close() ?>
             </div>
         </div>
     </main>
