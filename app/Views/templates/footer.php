f29a28fa76490d3625f24c8e13721b7fffe01d37
Juan Manuel Tola <tolajuanmanuel@yahoo.com.ar>
2020-10-17 03:47:15 -0300

    Agrego footer en front-app

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,3 +1,18 @@
+    <footer class="footer text-white text-center">  
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4"></div>
+                <div class="col-lg-4" id="copyright">
+                    <span style="text-align: center;"> Copyright © 2020 </span>
+                </div>
+                <div class="col-lg-4 text-center" id="footer-athomic">
+                    <span style="text-align: center;">developed by <a href="https://athomic.com.ar" target="_blank" style="color: #ccc1c1; font-weight: bolder; text-decoration: underline;">atho<span style="color: #d1c344;">mic</span></a></span>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
     <!-- Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
