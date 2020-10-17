f6ceceeaca267f5e25d1e47a1152e99dc9b0be0d
Juan Manuel Tola <tolajuanmanuel@yahoo.com.ar>
2020-10-17 04:22:26 -0300

    Cargo vistas de portfolio

--- app/Views/pages/portfolio.php
+++ app/Views/pages/portfolio.php
@@ -10,20 +10,14 @@
                 <div class="row">
 
                     <?php for ($i=0; $i < 6 ; $i++) { ?>
-                        <!-- <div class="col-12 col-sm-4 mb-4">
-                            <div class="card portfolio-card" style="background-image: url(assets/img/example1.jpg);">
-                                <div class="card-footer bg-light" style="margin-top: 250px;">
-                                <small class="text-muted">Last updated <?php echo $i?> mins ago</small>
-                                </div>
-                            </div>
-                        </div> -->
 
-                        <div class="col-12 col-sm-6 col-lg-4 my-3">
-                            <div class="gallery_image" style="background-image: url(<?php echo base_url();?>/assets/img/example1.jpg);"></div>
+                        <div class="col-12 col-sm-6 col-lg-4 my-3 gallery-object">
+                            <a class="text-white" href="#"><div class="gallery_image d-flex justify-content-center align-items-center" style="background-image: url(<?php echo base_url();?>/assets/img/example1.jpg);"></div></a>
                             <div class="card-footer">
-                                <small class="text-muted"> Titulo 1 </small>
+                                <small class=""> Titulo 1 </small>
                             </div>
                         </div>
+
                     <?php }  ?>
 
                 </div>
