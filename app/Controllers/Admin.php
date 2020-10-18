319700fe2db8759d8c145bcaf6a1541c716ae578
Juanma Tola <tolajuanmanuel@gmail.com>
2020-10-18 18:05:24 -0300

    Refactorizo metodos en controlador de admin y agrego SHA512 hash a password

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -1,6 +1,6 @@
 <?php namespace App\Controllers;
 
-use App\Models\LoginModel; //llamo al modelo que conecta con la base de usuario
+use App\Models\LoginModel;
 
 class Admin extends BaseController
 {
@@ -11,32 +11,12 @@ class Admin extends BaseController
 
 	public function index(){
 		session_start();
-
-		/*Si ya esta logueado, redirecciono a panel*/
 		if($this->sessionStatus()){
 			return redirect()->to(base_url().'/admin/panel/');
 			exit;
 		}
-		/*Sino muestro login*/ 
 		echo view('admin/login');
 	}
-
-	public function panel(){
-		session_start();
-
-		/*Si está logeado muestro panel*/
-		if ($this->sessionStatus()) {
-			/*Muestro panel de admin*/
-			echo view('admin/templates/header');
-			echo "<h1>BIENVENIDO A LA ADMINISTRACION<h1>";
-			echo view('admin/templates/footer');
-		}else{
-			/* Sino envio a login */
-			return redirect()->to(base_url().'/admin/?login=false');
-			exit;
-		}
-	}
-
 
 	public function login ($user = null, $pass = null){
 		/* Recibo datos del formulario */
@@ -45,31 +25,40 @@ class Admin extends BaseController
 			'username'=>$req->getPostGet('username'),
 			'password'=>$req->getPostGet('password')
 		);
-		/* Consulto datos de usuarios a db */
+		/* Obtengo usuario desde db */
 		$userModel = new LoginModel();
 		$userData = $userModel->getUser();
 		
-		/* Comparo datos recibidos con bd */
-		if($formData['username'] === $userData['0']['username'] && md5($formData['password']) === $userData['0']['password'])
-		{
-			//INICIAR SESION
+		if($this->userValidation($userData, $formData)){
+
 			session_start();
 			$_SESSION['id'] = $userData['0']['id'];
 			$_SESSION['username'] = $userData['0']['username'];
 			$_SESSION['password'] = $userData['0']['password'];
 
 			if ($this->sessionStatus()) {
-				//Luego redirigir a panel de admin
 				return redirect()->to(base_url().'/admin/panel/');
 				exit;
 			}else {
-				//Luego redirigir a login xq algo fallo
 				return redirect()->to(base_url().'/admin/?session_err');
 				exit;
 			}
 		}else {
-			//manejar error de login
 			return redirect()->to(base_url().'/admin/?login=data_err');
+		}
+	}
+
+	public function panel(){
+		session_start();
+
+		/*Si está logeado muestro panel*/
+		if ($this->sessionStatus()) {
+			echo view('admin/templates/header');
+			echo "<h1>BIENVENIDO A LA ADMINISTRACION<h1>";
+			echo view('admin/templates/footer');
+		}else{
+			return redirect()->to(base_url().'/admin/?login=false');
+			exit;
 		}
 	}
 
@@ -79,10 +68,21 @@ class Admin extends BaseController
 		return redirect()->to(base_url());
 	}
 
-	// Metodo q devuelve si el usuario esta conectado o no
-	public function sessionStatus() {
+	private function sessionStatus() {
 		$sessionStatus = session_status();
 		if ($sessionStatus === 2 && !empty($_SESSION['username']) && !empty($_SESSION['password'])) {
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	private function userValidation($userData = [], $formData= []) {
+
+		$entriedUsername = $formData['username'];
+		$entriedHashedPassword = hash('sha512', $formData['password']);
+
+		if ($entriedUsername === $userData['0']['username'] && $entriedHashedPassword === $userData['0']['password']) {
 			return true;
 		}else{
 			return false;
