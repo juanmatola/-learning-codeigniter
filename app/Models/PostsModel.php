ebc6e92e93de04365f2701952f9f716dffbc8614
Juanma Tola <tolajuanmanuel@gmail.com>
2020-10-20 19:38:34 -0300

    Primer esqueloto de controlador de post

--- app/Models/PostsModel.php
+++ app/Models/PostsModel.php
@@ -1,1 +1,18 @@
-//modelo par manejar los datos de posteos
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PostsModels extends Model
+{
+    /* Le digo al modelo sobre que tabla hacer las consultas y otras configuraciones sobre el modelo */
+    protected $table = 'is_post';
+    protected $primaryKey = 'id';
+    protected $returnType =  'array';
+    protected $allowedFields = []; //declaro sobre que dato de tabla voy a poder modificar desde el modelo
+
+    /* Metodo para traer todos los datos de usuario*/
+    public function getPosts()
+    {
+        return $this->findAll();
+    }
+}
