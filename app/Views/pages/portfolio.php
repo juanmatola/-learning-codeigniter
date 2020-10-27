05970ffd856d787d0b5b60f14e5a6db5f07f3671
Juanma Tola <tolajuanmanueel@gmail.com>
2020-10-27 17:36:20 -0300

    Muestro paginacion en vista de portfolio

--- app/Views/pages/portfolio.php
+++ app/Views/pages/portfolio.php
@@ -6,20 +6,21 @@
                 </div>
             </div>
         </div>
-            <div class="container" id="gallery">
-                <div class="row">
+        <div class="container" id="gallery">
+            <div class="row">
 
-                    <?php foreach ($posts as $post) { ?>
-                        <div class="col-12 col-sm-6 col-lg-4 my-3 gallery-object">
-                            <a class="text-white" href="#"><div class="gallery_image d-flex justify-content-center align-items-center" style="background-image: url(<?php echo base_url().'/writable/uploads/portfolio/'.$post['image']; ?>);"></div></a>
-                            <div class="card-footer">
-                                <small class=""><?php echo $post['title']; ?></small>
-                            </div>
+                <?php foreach ($posts as $post) { ?>
+                    <div class="col-12 col-sm-6 col-lg-4 my-3 gallery-object">
+                        <a class="text-white" href="#"><div class="gallery_image d-flex justify-content-center align-items-center" style="background-image: url(<?php echo base_url().'/writable/uploads/portfolio/'.$post['image']; ?>);"></div></a>
+                        <div class="card-footer">
+                            <small class=""><?php echo $post['title']; ?></small>
                         </div>
-                    <?php }  ?>
+                    </div>
+                <?php }  ?>
 
-
-                   
-                </div>
             </div>
+        </div>
+        <div class="container">
+            <?= $pager->links('group1', 'bootstrap_pager') ?>
+        </div>
     </section>
